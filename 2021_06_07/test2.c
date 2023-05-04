fff4d0caaf6cbb02538542b426ddd5a22475bc2e
ZJidong2001 <1436581890@qq.com>
2023-05-04 11:10:38 +0800

    2021_06_07

---
+++ 2021_06_07/test2.c
@@ -0,0 +1,13 @@
+//int g_val = 2021;
+
+//static int g_val = 2021;  // static修饰的全局变量
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+
+//static int Add(int x, int y)  // static修饰的函数
+//{
+//	return x + y;
+//}
